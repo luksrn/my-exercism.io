a0aad9a13ca58c29ecd90eb75a813dd1fa63ce6b
Lucas Farias <luksrn@gmail.com>
2016-01-17 12:13:07 -0300

    Update Hamming.java

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -4,10 +4,10 @@ import java.lang.IllegalArgumentException;
 public class Hamming {
 
 	public static int compute(String reference, String tested) {
-    if(reference.length() != tested.length()){
-      throw new IllegalArgumentException();
-    }
-    return (int) range(0, reference.length() )
-						.filter( i -> reference.charAt(i) != tested.charAt(i) ).count();
+		if(reference.length() != tested.length()){
+			throw new IllegalArgumentException();
+		}
+		return (int) range(0, reference.length() )
+				.filter( i -> reference.charAt(i) != tested.charAt(i) ).count();
 	}
 }
