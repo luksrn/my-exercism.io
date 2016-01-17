408392412443cb4e099818394470d2a7e03be5bd
Lucas Farias <luksrn@gmail.com>
2016-01-17 12:09:21 -0300

    Update Anagram.java

--- java/anagram/src/main/java/Anagram.java
+++ java/anagram/src/main/java/Anagram.java
@@ -1,2 +1,23 @@
 public class Anagram {
+  private Map<Character,Integer> statistics = new HashMap<>();
+	
+	public Anagram(String word) {
+		statistics = statisticsOf(word);
+	}
+	
+	private Map<Character, Integer> statisticsOf(String word){
+		return word.chars()
+				.mapToObj(e->(char)e)
+				.collect(toMap(Character::toLowerCase, e->1, Integer::sum));
+	}
+	
+	public List<String> match(List<String> words) {
+		return words.stream()
+					.filter( this::isAnagram )
+					.collect(toList());		
+	}
+		
+	private boolean isAnagram(String word) {
+		return statisticsOf(word).equals(statistics);		
+	}
 }
