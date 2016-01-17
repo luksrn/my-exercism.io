baba466ed001d14658a12634315bbf8745362abf
Lucas Farias <luksrn@gmail.com>
2016-01-17 12:13:53 -0300

    Update DNA.java

--- java/nucleotide-count/src/main/java/DNA.java
+++ java/nucleotide-count/src/main/java/DNA.java
@@ -1,2 +1,40 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class DNA {
+	
+	public static final Character ADENINE = 'A';
+	
+	public static final Character CYYOSINE = 'C';
+	
+	public static final Character GUANINE = 'G';
+	
+	public static final Character THYMINE = 'T';
+
+	private String nucleotides;
+	 
+	private Map<Character,Integer> nucleotideCounts = new HashMap<>();
+		
+	public DNA(String nucleotides) {
+		this.nucleotides = nucleotides;
+		nucleotideCounts.put(ADENINE, countNucleotide(ADENINE));
+		nucleotideCounts.put(CYYOSINE, countNucleotide(CYYOSINE));
+		nucleotideCounts.put(GUANINE, countNucleotide(GUANINE));
+		nucleotideCounts.put(THYMINE, countNucleotide(THYMINE));
+	}
+
+	private int countNucleotide(char c) {
+		return (int) nucleotides.chars().filter( nucleotide -> nucleotide == c ).count();
+	}
+	
+	public Integer count(char nucleotide) {
+		if ( !nucleotideCounts.keySet().contains(nucleotide) ){
+			throw new IllegalArgumentException("Invalid nucleotide!");
+		}
+		return nucleotideCounts.get(nucleotide);
+	}
+
+	public Map<Character,Integer> nucleotideCounts() {
+		return nucleotideCounts;
+	}
 }
