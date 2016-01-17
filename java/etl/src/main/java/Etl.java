fa91f6015332c066739cc7836204a2db5503e9e1
Lucas Farias <luksrn@gmail.com>
2016-01-17 12:11:02 -0300

    Update Etl.java

--- java/etl/src/main/java/Etl.java
+++ java/etl/src/main/java/Etl.java
@@ -3,6 +3,14 @@ import java.util.Map;
 
 public class Etl {
    public Map<String, Integer> transform(Map<Integer, List<String>> old) {
-      return null;
+      Map<String, Integer> transformedData = new HashMap<>();
+    
+		old.forEach((score, letters) -> {
+			letters.stream()
+				.map(String::toLowerCase)
+				.forEach(letter -> transformedData.put(letter, score));			
+		});
+	 
+		return transformedData;
    }
 }
