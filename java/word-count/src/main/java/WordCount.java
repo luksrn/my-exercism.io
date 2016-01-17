142066ef27febe4d4e0c5bcfaabd8bfcc3ca3e73
Lucas Farias <luksrn@gmail.com>
2016-01-17 12:14:52 -0300

    Update WordCount.java

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -1,2 +1,11 @@
+import java.util.Map;
+import java.util.stream.Stream;
+import static java.util.stream.Collectors.toMap;
+
 public class WordCount {
+
+	public Map<String, Integer> Phrase(String phrase) {
+		return Stream.of(phrase.split("\\s*[^a-zA-Z0-9]+\\s*"))
+							.collect(toMap(String::toLowerCase, e -> 1, Integer::sum));
+	}
 }
