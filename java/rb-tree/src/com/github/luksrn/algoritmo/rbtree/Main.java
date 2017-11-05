d04e3fd7e1adbbde4575595a706d59e8308b4dbc
Lucas Oliveira <luksrn@gmail.com>
2017-11-05 18:10:51 -0300

    WIP

--- java/rb-tree/src/com/github/luksrn/algoritmo/rbtree/Main.java
+++ java/rb-tree/src/com/github/luksrn/algoritmo/rbtree/Main.java
@@ -1,27 +1,51 @@
 package com.github.luksrn.algoritmo.rbtree;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-		RBTree<Integer> tree = new RBTree<>();
+		if( args.length != 1) {
+			System.err.println("USO: RBTree dicionario.txt");
+			return;
+		}
+		RBTree<String> rbtree = new RBTree<>();
+		carregarArvoreRBTree(rbtree, args[0]);
+		checkRBTree(rbtree);
+	}
 
-		tree.insert(100);
-		tree.insert(50);
-		tree.insert(25);
-		tree.insert(259);
-		tree.insert(89);
-		tree.insert(76);
-		
-		System.out.println("Minimum = " + tree.treeMinimum());
-		
-		tree.rbPrint();
-		tree.rbCheck();
 
-		System.out.println(tree.search(100));
-		System.out.println(tree.search(259));
-		System.out.println(tree.search(250));
-		
-		System.out.println(tree);
+	private static void carregarArvoreRBTree(RBTree<String> rbtree, String arquivo) {
+		try {
+			List<String> lines = Files.readAllLines(new File(arquivo).toPath());
+			for (String line : lines ) {
+				String[] tokens = line.split(" ");
+				String palavra = tokens[0];
+				String comando = tokens[1];
+				if( comando.equals("1") ) {
+					if( rbtree.search(palavra).data != null) {
+						System.out.println("Palavra " + palavra + " já foi adicionado na árvore.");
+						continue;
+					}
+					rbtree.insert(palavra);
+				} else if(comando.equals("0")) {
+					rbtree.delete(palavra);
+					//checkRBTree(rbtree);
+				}
+				checkRBTree(rbtree);
+			}
+		} catch (IOException  e) {
+			throw new RuntimeException("Falha ao carregar árvore RBTree. " + e.getMessage() , e);
+		}
+	}
+	
+
+	private static void checkRBTree(RBTree<String> rbtree) {
+		System.out.println("=== Checking ===");
+		rbtree.rbPrint();
+		rbtree.rbCheck();
 	}
 }
--- java/rb-tree/src/com/github/luksrn/algoritmo/rbtree/RBTree.java
+++ java/rb-tree/src/com/github/luksrn/algoritmo/rbtree/RBTree.java
@@ -1,28 +1,29 @@
 package com.github.luksrn.algoritmo.rbtree;
 
-import com.github.luksrn.algoritmo.rbtree.Node.Color;
+import com.github.luksrn.algoritmo.rbtree.RBElement.Color;
 
 public class RBTree <T extends Comparable<T>> {
 
-	private Node<T> root;
+	private RBElement<T> root;
 	
-	private Node<T> nil;
+	private RBElement<T> nil;
 	
 	{
-		nil = new Node<T>();
+		nil = new RBElement<T>();
 		nil.setColor(Color.BLACK);
+		root = nil;
 	}
 	
-	private boolean isNil(Node<T> node) {
+	private boolean isNil(RBElement<T> node) {
 		return node == null || nil == node;
 	}
 	public void insert(T z) {
-		insert(new Node<T>(z));
+		insert(new RBElement<T>(z));
 	}
 	
-	public void insert(Node<T> z) {
-		Node<T> y = nil;
-		Node<T> x = root;
+	public void insert(RBElement<T> z) {
+		RBElement<T> y = nil;
+		RBElement<T> x = root;
 		
 		while( !isNil(x) ) {
 			y = x;
@@ -39,17 +40,17 @@ public class RBTree <T extends Comparable<T>> {
 		} else {
 			y.right = z;
 		}
-		z.color = Node.Color.RED;
+		z.color = RBElement.Color.RED;
 		z.parent = y;
 		z.left = nil;
 		z.right = nil;
 		rbInsertFixup(z);
 	}
-	private void rbInsertFixup(Node<T> z) {
+	private void rbInsertFixup(RBElement<T> z) {
 		
 		while ( z.parent.color == Color.RED ) {
 			if( z.parent == z.parent.parent.left ) {
-				Node<T> y = z.parent.parent.right;
+				RBElement<T> y = z.parent.parent.right;
 				if( y.color == Color.RED ) {
 					z.parent.color = Color.BLACK;
 					y.color = Color.BLACK;
@@ -66,7 +67,7 @@ public class RBTree <T extends Comparable<T>> {
 				}
 			}
 			if( z.parent == z.parent.parent.right ) {
-				Node<T> y = z.parent.parent.left;
+				RBElement<T> y = z.parent.parent.left;
 				if( y.color == Color.RED ) {
 					z.parent.color = Color.BLACK;
 					y.color = Color.BLACK;
@@ -87,8 +88,8 @@ public class RBTree <T extends Comparable<T>> {
 		root.color = Color.BLACK;
 	}
 	
-	private void leftRotate(Node<T> x) {
-		Node<T> y = x.right;
+	private void leftRotate(RBElement<T> x) {
+		RBElement<T> y = x.right;
 		x.right = y.left;
 		if( !isNil(y.left)) {
 			y.left.parent = x;
@@ -105,8 +106,8 @@ public class RBTree <T extends Comparable<T>> {
 		x.parent = y;
 	}
 	
-	private void rightRotate(Node<T> x) {
-		Node<T> y = x.left;
+	private void rightRotate(RBElement<T> x) {
+		RBElement<T> y = x.left;
 		x.left = y.right;
 		if( !isNil(y.right)) {
 			y.right.parent = x;
@@ -123,7 +124,104 @@ public class RBTree <T extends Comparable<T>> {
 		x.parent = y;
 	}
 	
-	private void transplant(Node<T> u, Node<T> v) {
+	public boolean delete(T key) {
+		RBElement<T> node = search(key);
+		if(isNil(node)) {
+			return false;
+		}
+		delete(node);
+		return true;
+	}
+	
+	
+	public void delete(RBElement<T> z) {
+		RBElement<T> x = null;
+		RBElement<T> y = z;
+		Color originalColor = z.color;
+		if(isNil(z.left)) {
+			x = z.right;
+			transplant(z, z.right);
+		} else if(isNil(z.right)){
+			x = z.left;
+			transplant(z, z.left);
+		} else {
+			y = treeMinimum(z.right);
+			originalColor = y.color;
+			x = y.right;
+			if( y.parent == z ) {
+				x.parent = y;
+			} else {
+				transplant(z, y.right);
+				y.right = z.right;
+				y.right.parent = y;
+			}
+			transplant(z, y);
+			y.left = z.left;
+			y.left.parent = y;
+			y.color = z.color;
+		}
+			
+		if(originalColor.equals(Color.BLACK)) {
+			rbDeleteFixup(x);
+		}
+	}
+	
+	private void rbDeleteFixup(RBElement<T> x) {
+		while( !isNil(root) && x.color.equals(Color.BLACK)) {
+			if( x == x.parent.left ) {
+				RBElement<T> w = x.parent.right;
+				if(w.color.equals(Color.RED)) {
+					w.color = Color.BLACK;
+					x.parent.color = Color.RED;
+					leftRotate(x.parent);
+					w = x.parent.right;
+				}
+				if(w.left.color.equals(Color.BLACK) && w.right.color.equals(Color.BLACK)) {
+					w.color = Color.RED;
+					x = x.parent;
+				} else {
+					if (w.right.color.equals(Color.BLACK)) {
+						w.left.color = Color.BLACK;
+						w.color = Color.RED;
+						rightRotate(w);
+						w = x.parent.right;
+					}
+					w.color = x.parent.color;
+					x.parent.color = Color.BLACK;
+					w.right.color = Color.BLACK;
+					leftRotate(x.parent);
+					x = root;
+				}
+			} else if ( x == x.parent.right ) {
+				RBElement<T> w = x.parent.left;
+				if(w.color.equals(Color.RED)) {
+					w.color = Color.BLACK;
+					x.parent.color = Color.RED;
+					rightRotate(x.parent);
+					w = x.parent.left;
+				}
+				if(w.right.color.equals(Color.BLACK) && w.left.color.equals(Color.BLACK)) {
+					w.color = Color.RED;
+					x = x.parent;
+				} else {
+					if (w.left.color.equals(Color.BLACK)) {
+						w.right.color = Color.BLACK;
+						w.color = Color.RED;
+						leftRotate(w);
+						w = x.parent.left;
+					}
+					w.color = x.parent.color;
+					x.parent.color = Color.BLACK;
+					w.left.color = Color.BLACK;
+					rightRotate(x.parent);
+					x = root;
+				}
+			}
+		}
+		x.color = Color.BLACK;
+	}
+	
+	private void transplant(RBElement<T> u, RBElement<T> v) {
 		if( isNil(u.parent)) {
 			root = v;
 		} else if( u == u.parent.left ) {
@@ -136,12 +234,12 @@ public class RBTree <T extends Comparable<T>> {
 		}
 	}
 	
-	public Node<T> treeMinimum() {
+	public RBElement<T> treeMinimum() {
 		return treeMinimum(root);
 	}
 	
 	
-	private Node<T> treeMinimum(Node<T> r) {
+	private RBElement<T> treeMinimum(RBElement<T> r) {
 		if(isNil(r) || isNil(r.left)) {
 			return r;
 		}
@@ -150,10 +248,10 @@ public class RBTree <T extends Comparable<T>> {
 	public void rbPrint() {
 		printInOrder(root);
 	}
-	private void printInOrder(Node<T> node) {
+	private void printInOrder(RBElement<T> node) {
 		if(!isNil(node)) {
 			printInOrder(node.left);
-			System.out.print(" " + node.data.toString() + " ");
+			System.out.println(" " + node.data.toString() + " ");
 			printInOrder(node.right);
 		}
 	}
@@ -161,7 +259,7 @@ public class RBTree <T extends Comparable<T>> {
 	public void rbCheck() {
 		printRbCheck(root);
 	}
-	private void printRbCheck(Node<T> root) {
+	private void printRbCheck(RBElement<T> root) {
 		if(!isNil(root)) {
 			System.out.println("(" + root.parent + ", " + root.data.toString() + ", " + root.color + ", " + root.left+ ", " + root.right + ")");
 			printRbCheck(root.left);
@@ -169,10 +267,10 @@ public class RBTree <T extends Comparable<T>> {
 		}
 	}
 	
-	public Node<T> search(T key){
+	public RBElement<T> search(T key){
 		return search(root,key);
 	}
-	private Node<T> search(Node<T> root, T key) {
+	private RBElement<T> search(RBElement<T> root, T key) {
 		if(isNil(root) || root.data.compareTo(key) == 0 ) {
 			return root;
 		}
